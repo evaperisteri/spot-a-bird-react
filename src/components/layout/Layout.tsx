290028ab38488cc11d1928f1e94b2b0f37feb655
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-08-19 16:39:06 +0300

    grid row in layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,15 +4,19 @@ import Footer from "./Footer";
 
 export default function Layout() {
   return (
-    <div className="bg-[url('/images/ssspot.svg')] bg-cover bg-center flex flex-col min-h-screen">
-      <div className="bg-offwhite/70 h-20">
-        <Header />
-      </div>
-      <div className="h-75 flex-auto">
-        <Outlet />
-      </div>
-      <div className="h-6">
-        <Footer />
+    <div className="bg-[url('/images/ssspot.svg')] bg-cover bg-center min-h-screen">
+      <div className="grid grid-rows-[auto_1fr_auto] min-h-screen">
+        <div className="bg-offwhite/70 h-20">
+          <Header />
+        </div>
+        <div className=" overflow-auto">
+          <div className="mt-4 mx-auto w-full max-w-7xl sm:px-6 lg:px-8">
+            <Outlet />
+          </div>
+        </div>
+        <div className="">
+          <Footer />
+        </div>
       </div>
     </div>
   );
